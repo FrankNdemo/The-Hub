94355f15aa1abdf97315a6ad341b2223a4d9761b
😂 <you@example.com>
2026-06-21 09:09:28 +0300

    updated talk with us

--- wellness/tailwind.config.ts
+++ wellness/tailwind.config.ts
@@ -94,8 +94,8 @@ export default {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "mobile-contact-glow": "mobile-contact-glow 6.8s ease-in-out infinite",
-        "mobile-contact-bounce": "mobile-contact-bounce 5.6s ease-in-out infinite",
+        "mobile-contact-glow": "mobile-contact-glow 8.8s ease-in-out infinite",
+        "mobile-contact-bounce": "mobile-contact-bounce 8.8s ease-in-out infinite",
       },
     },
   },
